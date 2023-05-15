6b3189f0909fcacd0a135025fbea8f31f1b936d4
shnorlaxzz <mshnas56@gmail.com>
2023-05-15 21:54:31 +0000

    docstring update

--- stack-and-queue/pseudo_queue.py
+++ stack-and-queue/pseudo_queue.py
@@ -1,13 +1,20 @@
 from stack import Stack
-class PsuedoQueue:
+
+
+class PseudoQueue:
+    """A class representing a Queue implemented using two Stacks."""
+
     def __init__(self):
+        """Initialize an empty pseudo queue."""
         self.stack1 = Stack()
         self.stack2 = Stack()
 
     def enqueue(self, value):
+        """Add an element to the rear of the pseudo queue."""
         self.stack1.push(value)
 
     def dequeue(self):
+        """Remove and return the element from the front of the pseudo queue."""
         if self.stack1.is_empty() and self.stack2.is_empty():
             raise Exception("Queue is empty")
         elif self.stack2.is_empty():
@@ -16,5 +23,5 @@ class PsuedoQueue:
         return self.stack2.pop()
 
     def __str__(self):
+        """Return a string representation of the pseudo queue."""
         return str(self.stack1)
-    
