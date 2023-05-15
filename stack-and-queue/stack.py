b2b78614a4ab1f6667792e8779a312f715862d30
shnorlaxzz <mshnas56@gmail.com>
2023-05-15 21:51:29 +0000

    docstring updated

--- stack-and-queue/stack.py
+++ stack-and-queue/stack.py
@@ -2,11 +2,15 @@ from node import Node
 
 
 class Stack:
+    """A class representing a Stack using a linked list."""
+
     def __init__(self):
+        """Initialize an empty stack."""
         self.top = None
         self.size = 0
 
     def push(self, value):
+        """Add an element to the top of the stack."""
         node = Node(value)
         if self.top:
             node.next = self.top
@@ -14,6 +18,7 @@ class Stack:
         self.size += 1
 
     def pop(self):
+        """Remove and return the element from the top of the stack."""
         if not self.top:
             raise ValueError("Empty Stack")
         else:
@@ -23,14 +28,15 @@ class Stack:
             return temp.value
 
     def peek(self):
+        """Return the element at the top of the stack without removing it."""
         if not self.top:
             raise ValueError("Empty Stack")
         else:
             return self.top.value
 
     def is_empty(self):
+        """Check if the stack is empty."""
         if not self.top:
             return True
         else:
             return False
-
