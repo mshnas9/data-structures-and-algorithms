8cf6ed66fefb35701677ad316dfc6aca5eed8db3
shnorlaxzz <mshnas56@gmail.com>
2023-05-20 20:37:24 +0000

    init commit

--- stack-and-queue/main.py
+++ stack-and-queue/main.py
@@ -1,10 +1,17 @@
 from queue import Queue
 
-queue1 = Queue()
-queue1.enqueue(88)
-queue1.enqueue(11)
-queue1.enqueue(22)
+def DuckDuckGoose(k, arr):
+    queue1 = Queue()
+    for i in arr:
+        queue1.enqueue(i)
+    while queue1.front.next:
+        for i in range(k):
+            queue1.enqueue(queue1.dequeue())
+        queue1.dequeue()
+    return queue1.front.value
 
-# queue1.enqueue(1)
-print(queue1.dequeue())
-print(queue1.peek())
+
+print (DuckDuckGoose(3, ['A', 'B', 'C', 'D', 'E']))
+
+
+
