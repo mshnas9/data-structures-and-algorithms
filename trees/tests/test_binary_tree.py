255179617c5e7b73eba86d567923be7f837b1fe8
shnorlaxzz <mshnas56@gmail.com>
2023-05-21 05:57:23 +0000

    tests remaining

--- trees/tests/test_binary_tree.py
+++ trees/tests/test_binary_tree.py
@@ -1,9 +1,3 @@
 import pytest
 from binary_tree import BinaryTree
 
-# Raise value error if empty tree
-def test_empty_tree():
-    
-    tree = BinaryTree()
-    with pytest.raises(ValueError):
-        tree.pre_order()
